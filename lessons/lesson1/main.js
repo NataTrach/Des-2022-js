c1fd73536fa98099d384438fb0ae2ffd9cce106c
NataTrach <natalitrach12@gmail.com>
2023-02-07 14:12:01 +0200

    js lesson1

---
+++ lessons/lesson1/main.js
@@ -0,0 +1,65 @@
+// console.log(33);
+
+// document.write('Hello')
+
+// let strText = 'Hello';// type string 'Hello' "Hello" `Hello`
+// console.log(strText);
+
+// const PI = 3.14;
+// console.log(PI);
+
+// let str = 'Hello';
+// console.log(typeof str);
+//
+// let num = 10;
+// console.log(typeof num);
+//
+// let bool = true;
+// console.log(typeof bool);
+
+// let str = '"Hello"';
+// console.log(str);
+//
+// let str2 = "World";
+// console.log(str2);
+
+// let str = 'Hello';
+// let str2 = "World";
+// // console.log(str+str2);
+// // console.log(str+' '+str2);
+// let str3 = 'Love';
+// console.log(str+' '+str2+' '+str3);
+// console.log(`${str} ${str2} ${str3}`)
+
+// let num = 10;
+// console.log(num);
+//
+// let num1 = 3;
+// console.log(num1);
+//
+// console.log(num+num1);
+// console.log(num*num1);
+// console.log(num/num1);
+// console.log(num-num1);
+// console.log(num%num1);
+
+// console.log(0.1+0.2);
+//
+// let big = BigInt(12345678);
+// console.log(big);
+
+let a = true;
+let b = false;
+console.log(typeof a);
+console.log(typeof b);
+
+//> < >= <= != == === !==
+
+// let res = 5 < 7;
+// console.log(res);
+//
+// console.log(10 === 10);
+// console.log(10 !==  10);
+// console.log(10 !== '10');
+// console.log(10 == '10');
+// console.log(10 === '10');
