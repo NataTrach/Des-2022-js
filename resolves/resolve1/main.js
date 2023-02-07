060d57338fcb4c7956951cbdaf5de35119e95c21
NataTrach <natalitrach12@gmail.com>
2023-02-07 15:09:08 +0200

    js lesson1

--- resolves/resolve1/main.js
+++ resolves/resolve1/main.js
@@ -13,9 +13,10 @@
 
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не ' +
 // 'об'єкт, просто за допомоги конкатенації)
-// let firstName = 'Natasha';
-// let middleName = 'Fedorivna';
-// let lastName = 'Trach';
+
+let firstName = 'Natasha';
+let middleName = 'Fedorivna';
+let lastName = 'Trach';
 //
 // let res = firstName +' '+ middleName + ' ' + lastName;
 // console.log(res);
@@ -27,21 +28,20 @@
 
 
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
-let a = 100;
-let b = '100';
-let c = true;
-
-console.log(typeof a);
-console.log(typeof b);
-console.log(typeof c);
-
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+//
 
 
 
 // Додаткове для тих хто цікавився prompt`oм
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в
 //  консоль
-
 
 
 
