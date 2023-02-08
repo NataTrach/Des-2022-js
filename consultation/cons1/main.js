45223832f3a2fb19d6a51443ab7655ccbcc5dfad
NataTrach <natalitrach12@gmail.com>
2023-02-08 13:07:52 +0200

    js lesson1

---
+++ consultation/cons1/main.js
@@ -0,0 +1,56 @@
+// let  name = 'Mariya';
+// let obj ={
+//     age: 23
+// }
+// obj.name = name;
+// console.log(obj);
+
+// let str = 'Hello';
+// switch (str) {
+//     case 'Hello':
+//         console.log('Hello world');
+//         break;
+//     case 1:
+//         console.log('Error');
+//         break;
+//     default:
+//         console.log('?????');
+// }
+
+// let str = 'a'> 'A';
+// console.log(str);
+
+// let day = 3;
+// if (day>0 && day<10) {
+//     console.log('+');
+// } else {
+//     console.log('-');
+// }
+
+// let obj = {
+//     name:'Vasya',
+//     age:22,
+//     status:true,
+//     skills:['js','java'],
+//     authors:[
+//         {name: 'Roma'},
+//         {name: 'Petya'}
+//     ]
+// }
+// console.log(obj.name,obj.age);
+// console.log(obj.skills,obj.authors);
+
+// let students = {
+//     st1:{name: 'Anna', age:20},
+//     st2:{name: 'Olga', age:25},
+//     st3:{},
+// }
+// students.st3.name = 'Vasya';
+// console.log(students);
+
+let arr = [1,2,3,4];
+arr[arr.length-1] =5;
+
+console.log(arr);
+
+// console.log(arr.length-2);
