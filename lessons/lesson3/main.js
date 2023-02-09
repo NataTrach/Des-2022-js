bd88fcf0f5d136e59dfa9acb6d0a661dbd2e7536
NataTrach <natalitrach12@gmail.com>
2023-02-09 14:50:13 +0200

    js lesson1

---
+++ lessons/lesson3/main.js
@@ -0,0 +1,80 @@
+// let x = 0;
+// x++;
+// console.log(x);
+//
+// x--;
+// console.log(x);
+//
+// let y = 0;
+// y = y+1;
+// console.log(y);
+//
+// y +=1;
+// console.log(y);
+
+// let x = 0;
+// let y = ++x;
+// console.log(y);
+
+
+
+// *********** short head
+// for (let i =0;i <users.length; i++) {
+//     console.log(i);
+// }
+
+// itar+Tab //
+// ritar+Tab//
+
+// document.write(`<h2>Hello</h2>`);
+// document.write(`<h2>${simpson.name}</h2>`);
+
+// document.write(`
+//     <div>
+//         <h2>${simpson.name} ${simpson.surname}</h2>
+//         <img src="${simpson.photo}">
+//         <p>${simpson.info}</p>
+//     </div>
+// `)
+//************
+
+//vid pochatky do kincya
+// iter+Tab
+// for (let simpsonMember of simpsons) {
+//     document.write(`
+// //     <div>
+// //         <h2>${simpson.name} ${simpson.surname}</h2>
+// //         <img src="${simpson.photo}">
+// //         <p>${simpson.info}</p>
+// //     </div>
+// // `)
+// }
+// ************
+
+       // console.log(coursesItem.modules);//
+// let modules = coursesItem.modules;
+// console.log(modules);
+// for (const module = of modules) {
+//     console.log(module);
+// }
+
+
+
+// let user = {
+//     name:'Vasya',
+//     age: 33,
+//     status: true
+// };
+
+
+// // itin//
+
+
+// for (let key in user) {
+//     console.log(key);
+//
+// }
+
+
+
+
