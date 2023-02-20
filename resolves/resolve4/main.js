c611947c38d57be19111019ca9b53d3f1182d082
NataTrach <natalitrach12@gmail.com>
2023-02-20 15:17:30 +0200

    res4

--- resolves/resolve4/main.js
+++ resolves/resolve4/main.js
@@ -70,17 +70,28 @@
 // - створити функцію яка створює ul з li. Текст li задати через аргумент всім однаковий.
 //     Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
-function liCreat(text,count) {
-    document.write(`<ul>`);
-    for (let i = 0; i < count; i++){
-        document.write(`<li>${text}</li>`);
-    }
-    document.write(`</ul>`)
-}
-liCreat ('text',4);
+// function liCreat(text,count) {
+//     document.write(`<ul>`);
+//     for (let i = 0; i < count; i++){
+//         document.write(`<li>${text}</li>`);
+//     }
+//     document.write(`</ul>`)
+// }
+// liCreat ('text',4);
 
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+
+// const arrey = [1,2,'bybyby',true,[5,10]];
+// function listCreator(arr) {
+//     document.write('<ul>');
+//     for (const item of arr) {
+//         document.write(`<li>${item}</li>`);
+//     }
+//     document.write(`</ul>`);
+// }
+// listCreator(arrey);
 
 
 
@@ -88,16 +99,86 @@ liCreat ('text',4);
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ.' +
 // ' Для кожного об'єкту окремий блок.
 
+// function objBlockCreator(objects) {
+//     for (const item of objects) {
+//         document.write(`<div>${item.id}${item.name}${item.age}</div>`);
+//     }
+// }
+
+
 
 // - створити функцію яка повертає найменьше число з масиву
+
+// function minName(numbers) {
+//     let min = numbers[0];
+//     for (const number of numbers) {
+//         if (min > number) {
+//             min = number
+//         }
+//     }
+//     return min;
+// }
+// minName([-54,32,-11,6]);
+
+
 
 
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 //     Приклад sum([1,2,10]) //->13
 
+// function sum(numbers) {
+//     let bascket = 0;
+//     for (const number of numbers) {
+//         bascket += number;
+//     }
+//     return bascket;
+// }
+// console.log(sum([1,2,10]));
+
 
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 
+// function swap(arr,index1,index2) {
+//     let v1 = arr[index1];
+//     let v2 = arr[index2];
+//     arr[index1] = v2;
+//     arr[index2] = v1;
+//     return arr;
+// }
+//
+// console.log(swap([11,12,13,14],0,4));
+
+
+
+
 //
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+
+// function exchange(uah,currencies,exitCurrency) {
+//     for (const item of currencies) {
+//         if (item.currency === exitCurrency) {
+//             return uah / item.value;
+//         }
+//     }
+// }
+//
+// console.log(exchange(
+//     10000,
+//     [
+//         {currency: 'USD', value: 40},
+//         {currency: 'EUR', value: 42},
+//         {currency: 'GBP', value: 43},
+//     ],
+//     'USD'
+// ));
+
+
+
+
+
+
+
+
+// ***********
+// .log
